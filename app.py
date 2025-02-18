a5b3d2de53fbf31d8282f0a03dae13e8337eed55
timslo-gift <manedouard@gmail.com>
2025-02-18 03:46:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+port = int(os.environ.get("PORT", 10000))  # Utilise le port défini par Render ou 10000 par défaut
+app.run(host="0.0.0.0", port=port)
